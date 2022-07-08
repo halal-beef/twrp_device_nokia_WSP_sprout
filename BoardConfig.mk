53c28aa69319f5e8d1a75518f2644a9859954c46
halal-beef <78730004+halal-beef@users.noreply.github.com>
2022-07-08 22:11:46 +0100

    Add files via upload

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,4 @@
-LOCAL_PATH := device/nokia/WSP_sprout
+LOCAL_PATH := device/hmd/WSP_sprout
 
 # Architecture
 TARGET_ARCH := arm64
@@ -15,9 +15,7 @@ TARGET_CPU_ABI_LIST_64_BIT := $(TARGET_CPU_ABI)
 TARGET_CPU_ABI_LIST_32_BIT := $(TARGET_2ND_CPU_ABI),$(TARGET_2ND_CPU_ABI2)
 TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI_LIST_64_BIT),$(TARGET_CPU_ABI_LIST_32_BIT)
 
-TARGET_BOARD_PLATFORM := mt6761               # From build.prop in /vendor (not /system)
-#TARGET_NO_BOOTLOADER := true
-#TARGET_BOOTLOADER_BOARD_NAME := d937           # as above
+TARGET_BOARD_PLATFORM := mt6761
 TARGET_BOARD_SUFFIX := _64
 TARGET_USES_64_BIT_BINDER := true             
 
@@ -29,12 +27,11 @@ BOARD_HAS_MTK_HARDWARE := true
 TARGET_IS_64_BIT := true
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/zImage
 TARGET_PREBUILT_RECOVERY_KERNEL := $(LOCAL_PATH)/prebuilt/zImage
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 # buildvariant=user
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 
 BOARD_KERNEL_BASE := 0x40078000                                                
 BOARD_KERNEL_PAGESIZE := 2048                                                  
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x11a88000 --tags_offset 0x07808000   # --ramdisk_offset 0x03f88000 --tags_offset 0x0df88000
-
-TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x11a88000 --tags_offset 0x07808000
+#TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
 TARGET_RECOVERY_INITRC := $(LOCAL_PATH)/recovery/root/init.recovery.mt6761.rc
 
 
@@ -52,6 +49,9 @@ BOARD_VENDORIMAGE_PARTITION_SIZE := 0x30000000
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
 #More Board settings
+
+#BOARD_USES_RECOVERY_AS_BOOT := true
+
 BOARD_HAS_NO_REAL_SDCARD := true
 BOARD_USES_MMCUTILS := true
 BOARD_SUPPRESS_EMMC_WIPE := true
@@ -62,7 +62,6 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery.fstab
-#BOARD_USES_FULL_RECOVERY_IMAGE := true
 RECOVERY_SDCARD_ON_DATA := true 
 BOARD_RECOVERY_SWIPE := true
 
@@ -71,8 +70,7 @@ DEVICE_RESOLUTION := 720x1520
 DEVICE_SCREEN_WIDTH := 720
 DEVICE_SCREEN_HEIGHT := 1520
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
-#below tagged cause mtp didnt work anyways
-#TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun%d/file
+
 TARGET_RECOVERY_LCD_BACKLIGHT_PATH := "/sys/class/leds/lcd-backlight/brightness\"
 TARGET_DISABLE_TRIPLE_BUFFERING := false
 TW_THEME := portrait_hdpi
@@ -82,16 +80,8 @@ TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_MAX_BRIGHTNESS := 255
 TW_NO_USB_STORAGE := false
 
-#DATA PARTITION NOT MOUNTED AS NOT POSSIBLE TO DECRYPT
-#COMMENTED OUT IN recovery.fstab TOO
-#
-#TW_INTERNAL_STORAGE_PATH := "/data/media"
-#TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
-#TW_INCLUDE_CRYPTO := true
-#TW_CRYPTO_FS_TYPE := "ext4"
-#TW_CRYPTO_REAL_BLKDEV := "/dev/block/by-name/userdata"
-#TW_CRYPTO_MNT_POINT := "/data"
-#TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,formattable,data=ordered"
+TW_Y_OFFSET := 60
+TW_H_OFFSET := -60
 
 # Crypto
 TW_INCLUDE_CRYPTO := true
@@ -110,40 +100,25 @@ TW_INTERNAL_STORAGE_PATH := "/data/media/0"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
-#TW_NO_REBOOT_BOOTLOADER := false
 TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_NO_USB_STORAGE := false
 TW_INPUT_BLACKLIST := "hbtp_vm"
 HAVE_SELINUX := true
-
-BOARD_USES_RECOVERY_AS_BOOT := true
-
+TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_USE_TOOLBOX := true
 TW_EXTRA_LANGUAGES := true
-TW_DEFAULT_LANGUAGE := en
 TW_NO_SCREEN_BLANK := true
-TW_NO_BATT_PERCENT := false
 TW_EXCLUDE_SUPERSU := true
 TW_INCLUDE_NTFS_3G := true     
 TW_INCLUDE_FUSE_EXFAT := true
-TW_NO_SCREEN_TIMEOUT := true
 TWRP_INCLUDE_LOGCAT := true
 TW_INCLUDE_FB2PNG := true                     # Include Screenshot Support 
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
 TW_SECONDARY_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
 TW_DEFAULT_BRIGHTNESS := 80
-
-
-TW_Y_OFFSET := 60
-TW_H_OFFSET := -60
-
-# Encryption
-PLATFORM_SECURITY_PATCH := 2099-12-31
-PLATFORM_VERSION := 16.1.0
-VENDOR_SECURITY_PATCH := 2099-12-31
 #
 #
 #The End!
